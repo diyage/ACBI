38a9d93fb11e3a80a1845202516fa481be07153f
diyage <diyage0418@163.com>
2022-12-23 16:15:47 +0800

    finish ACBI

--- demo.py
+++ demo.py
@@ -3,8 +3,18 @@ import torch
 
 
 model = Cifar10ModelForGetFeature(256)
+"""
+please make sure this model has been already trained well.
+"""
 x = torch.rand(size=(32, 3, 32, 32))
 y = torch.randint(0, 10, size=(32, ))
+"""
+y is the real target
+"""
+
+clean_predict: torch.Tensor = model(x)
+clean_acc = (clean_predict.argmax(dim=-1) == y).float().mean()
+
 attacker = ACBI_PGD(
     model,
     eps=8.0/255,
@@ -12,4 +22,8 @@ attacker = ACBI_PGD(
     init_num_for_v=2,
 )
 x_adv = attacker(x, y)
-print(x_adv)
+adv_predict: torch.Tensor = model(x_adv)
+adv_acc = (adv_predict.argmax(dim=-1) == y).float().mean()
+
+print('Clean Accuracy: {:.3%}'.format(clean_acc.item()))
+print('Adversarial Accuracy: {:.3%}'.format(adv_acc.item()))
